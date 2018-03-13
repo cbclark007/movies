55e2c3e6382a1f2882a14c7196e0d2aafa5a6e7d
Christopher Clark <cbclark007@gmail.com>
2018-03-12 21:50:22 -0700

    another commit?

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-  
+  const titleInput = $('#movie-title');
+  const poster = $('#poster');
+  const errorMessage = $('#error-message')
+  titleInput.on('keyup', function(e) {
+    if(e.key === 'Enter') {
+      const movieTitle = titleInput.val();
+      titleInput.val('');
+      $.get(`/movieInfo?title=${movieTitle}`, (response) => {
+        if(response.poster) {
+          errorMessage.addClass('hidden');
+          poster.removeClass('hidden');
+          poster.attr('src', response.poster);
+        } else {
+          errorMessage.removeClass('hidden');
+          errorMessage.text(`Could not locate the poster for ${movieTitle}`);
+          poster.addClass('hidden');
+        }
+
+      })
+      // window.location.replace('/movie');
+    }
+  })
 })
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const axios = require('axios');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.set('views', path.join(__dirname, "views"));
@@ -13,6 +15,27 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
-app.listen(3000, ()=>{
+app.get('/movieInfo', (req, res) => {
+  const title = req.query.title;
+  const apiKey = process.env.API_KEY;
+  console.log(apiKey);
+  axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`)
+    .then((response) => {
+      const poster = response.data.Poster;
+      res.send({poster});
+    })
+    .catch((response) => {
+      console.log(response);
+      res.send({});
+    });
+  //end of axios
+
+})
+
+app.listen(port, () => {
   console.log("Server is up on 3000")
 })
+
+
+//mongod.exe --dbpath C:\Users\PTC-Student-38\mongo-data
+//killall mongod
